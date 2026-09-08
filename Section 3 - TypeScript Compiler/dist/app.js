72e8d3bc829658022bfe8db5a93911b4fddf1c4d
sorgerator <tarik.sorguc1@gmail.com>
2026-09-08 13:57:26 +0200

    Submitting Section 3

---
+++ Section 3 - TypeScript Compiler/dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName;
+userName = "Tarik";
+console.log(userName);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
+//# sourceMappingURL=app.js.map
---
+++ Section 3 - TypeScript Compiler/src/app.ts
@@ -0,0 +1,15 @@
+import fs from "node:fs";
+
+// fs.readFileSync()
+
+let userName: string;
+
+userName = "Tarik";
+
+console.log(userName);
+
+function add(a: any, b: any) {
+  return a + b;
+}
+
+console.log(add(1, 2));
